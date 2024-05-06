3e05d11624ded51a24b25b35afb8cf5e29cc0491
Balbonger <154312266+Balbonger@users.noreply.github.com>
2024-05-06 10:59:16 +0800

    Windows App Forms / C#

---
+++ Online Inventory System with Barcode and Excel  Export/ChangelogData.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NibsInventoryManagementSystem
+{
+    internal class ChangelogData
+    {
+        //public int ID { set; get; } //0
+
+        public string LogID { set; get; } //1
+
+        public string Activity { set; get; } //2
+
+        public string Destination { set; get; } //3
+
+        public string Personnel { set; get; } //4
+
+        public string Client { set; get; } //5
+
+        public string User { set; get; } //6
+
+        public string Date { set; get; } //7
+
+
+
+
+        public List<ChangelogData> AllChangelogData()
+        {
+            List<ChangelogData> listData = new List<ChangelogData>();
+            using (MySqlConnection connect = new MySqlConnection(Form1.gateway))
+            {
+                connect.Open();
+
+                MySqlCommand direct = new MySqlCommand(Form1.database, connect);
+                direct.ExecuteNonQuery();
+
+
+                string selectData = "SELECT * FROM changelog ORDER BY id DESC";
+
+                using (MySqlCommand cmd = new MySqlCommand(selectData, connect))
+                {
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        ChangelogData apData = new ChangelogData();
+                        //apData.ID = (int)reader["id"];
+                        apData.LogID = reader["log_id"].ToString();
+                        apData.Activity = reader["prod_name"].ToString();
+                        apData.Destination = reader["destination"].ToString();
+                        apData.Personnel = reader["person_incharge"].ToString();
+                        apData.Client = reader["client"].ToString();
+                        apData.User = reader["log_user"].ToString();
+                        apData.Date = reader["log_date"].ToString();
+                        
+
+                        listData.Add(apData);
+                    }
+                }
+            }
+
+            return listData;
+        }
+    }
+}
---
+++ Online Inventory System with Barcode and Excel  Export/RecoveryPassword.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NibsInventoryManagementSystem
+{
+    public partial class RecoveryPassword : UserControl
+    {
+        public RecoveryPassword()
+        {
+            InitializeComponent();
+        }
+
+        private void restore_passConfirm_Click(object sender, EventArgs e)
+        {
+            if(restore_password.Text == "")
+            {
+                MessageBox.Show("Empty fields.", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (restore_password.Text =="restore2024")
+                {
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Incorrect Password.", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void restore_passClear_Click(object sender, EventArgs e)
+        {
+            restore_password.Text = "";
+        }
+
+        private void restore_password_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                restore_passConfirm_Click((object)sender, e);
+
+            }
+        }
+
+        private void RecoveryPassword_Load(object sender, EventArgs e)
+        {
+            colorManagement();
+        }
+
+        public void colorManagement()
+        {
+            restore_passConfirm.BackColor = Form1.appC;
+        }
+    }
+}
